c5408c3cbf53993d10cd695cdd1bd6b437b582e5
ExizMan <yurasankin@yandex.ru>
2024-07-06 17:06:58 +0300

    cors

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def add_cors_headers(request, call_next):
     response = Response()
 
     if request.method == "OPTIONS":
-        response.headers["Access-Control-Allow-Origin"] = "http://172.17.0.1:5173"
+        response.headers["Access-Control-Allow-Origin"] = "http://192.168.17.119:5173"
         response.headers["Access-Control-Allow-Credentials"] = "true"
         response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
         response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
@@ -38,7 +38,7 @@ async def add_cors_headers(request, call_next):
 
     #origin = request.headers.get("Origin")
     #if origin and origin in ["http://localhost:5173", "http://192.168.0.1:5173", "http://172.17.0.1:5173"]:
-    response.headers["Access-Control-Allow-Origin"] = "http://172.17.0.1:5173"
+    response.headers["Access-Control-Allow-Origin"] = "http://192.168.17.119:5173"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
